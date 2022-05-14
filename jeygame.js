b02ee9472ba57d357e3efb79ece784b30772c020
Vercte <80974914+lopste@users.noreply.github.com>
2022-05-13 17:08:41 -0700

    Add files via upload

--- jeygame.js
+++ jeygame.js
@@ -32,11 +32,25 @@ const Jeygame = function(find) {
 Jeygame.element = undefined;
 Jeygame.instances = [];
 Jeygame.keysHeld = {};
+
+const __jg_errors = {
+    "noRendering": "Jeygame has no rendering element! Use Jeygame.init()",
+    "notRendered": "Instance is not being rendered!"
+}
+
 Jeygame.components = {
     "2d": {
         "properties": {
             "rendered": "true",
             "size": {"width": 10, "height": 10},
+            "resize": function(x,y) {
+                this.size.width = x, this.size.width = y;
+                this.element.style.setProperty("width", `${x}px`);
+                this.element.style.setProperty("height", `${y}px`);
+                return this;
+            },
+            "moveTo": function(x,y) {this.position.x = x, this.position.y = y; return this;},
+            "moveBy": function(x,y) {this.position.x += x, this.position.y += y; return this;},
             "element": document.createElement("div")
         },
         "init": function(instance) {
@@ -73,7 +87,7 @@ Jeygame.components = {
                 instance.element.style.setProperty("top", "0px");
                 Jeygame.element.append(instance.element);
             } else {
-                throw new Error("Jeygame has no rendering element! Use Jeygame.init()");
+                throw new Error(__jg_errors.noRendering);
             }
         },
     },
@@ -90,22 +104,81 @@ Jeygame.components = {
             if(instance.components.indexOf("2d") != -1) {
                 instance.element.style.setProperty("background-color", "#008cff");
             } else {
-                throw new Error("Instance is not being rendered!");
+                throw new Error(__jg_errors.notRendered);
             }
         }
     },
     "rotate": {
         "properties": {
             "rotation": 0,
-            "rotate": function(rot) {
+            "rotateTo": function(rot) {
                 this.rotation = rot % 360;
+                this.element.style.setProperty("transform", `rotate(${this.rotation}deg)`);
+                return this;
+            },
+            "rotateBy": function(rot) {
+                this.rotation += rot;
+                this.rotation = this.rotation % 360;
                 this.element.style.setProperty("transform", `rotate(${this.rotation}deg)`);
                 return this;
             }
         },
         "init": function(instance) {
             if(instance.components.indexOf("2d") == -1) {
-                throw new Error("Instance is not being rendered!");
+                throw new Error(__jg_errors.notRendered);
+            }
+        }
+    },
+    "border": {
+        "properties": {
+            "borderColorSet": "black",
+            "borderWidthSet": 2,
+            "borderStyleSet": "solid",
+            "borderRadiusSet": 0,
+            "borderColor": function(clr) {
+                this.borderColorSet = clr;
+                this.element.style.setProperty("border-color", clr);
+                return this;
+            },
+            "borderWidth": function(wid) {
+                this.borderWidthSet = wid;
+                this.element.style.setProperty("border-width", `${wid}px`);
+                return this;
+            },
+            "borderStyle": function(sty) {
+                this.borderStyleSet = sty;
+                this.element.style.setProperty("border-style", sty);
+                return this;
+            },
+            "borderRadius": function(rad) {
+                this.borderRadiusSet = rad;
+                this.element.style.setProperty("border-radius", `${rad}px`);
+                return this;
+            }
+        },
+        "init": function(instance) {
+            if(instance.components.indexOf("2d") != -1) {
+                instance.element.style.setProperty("border-color", "black");
+                instance.element.style.setProperty("border-width", "2px");
+                instance.element.style.setProperty("border-style", "solid");
+            } else {
+                throw new Error(__jg_errors.notRendered);
+            }
+        }
+    },
+    "css": {
+        "properties": {
+            "setCSSProperty": function(prop, val) {
+                this.element.style.setProperty(prop,val)
+                return this;
+            },
+            "getCSSProperty": function(prop) {
+                return this.element.style.getPropertyValue(prop);
+            }
+        },
+        "init": function(instance){
+            if(instance.components.indexOf("2d") == -1) {
+                throw new Error(__jg_errors.notRendered);
             }
         }
     }
@@ -165,7 +238,7 @@ class JeygameInstance {
                 if(Jeygame.components[components[i]].init) {
                     Jeygame.components[components[i]].init(this)
                 }
-                returnInstance.components.push(componentList[i])
+                this.components.push(components[i])
             }
         }
         return this;
@@ -215,7 +288,9 @@ Object.defineProperties(Jeygame, {
             jgElement.style.setProperty("width", `${sizeX}px`);
             jgElement.style.setProperty("height", `${sizeY}px`);
             jgElement.style.setProperty("position", "relative");
+            jgElement.style.setProperty("background-color", "#fff");
             jgElement.style.setProperty("overflow", "hidden");
+            jgElement.style.setProperty("outline", "none");
             jgElement.setAttribute("tabindex", "0")
             Jeygame.element = jgElement;
             jgElement.addEventListener("keydown", ({key}) => {
@@ -237,6 +312,13 @@ Object.defineProperties(Jeygame, {
             jgElement.addEventListener("blur", () => {
                 Jeygame.keysHeld = {};
                 Jeygame.fire("unfocus");
+            })
+            jgElement.addEventListener("focus", () => {
+                Jeygame.keysHeld = {};
+                Jeygame.fire("focus");
+            })
+            window.addEventListener("resize", () => {
+                Jeygame.fire("resizeWindow", window.innerWidth, window.innerHeight);
             })
             return jgElement;
         }
@@ -269,5 +351,85 @@ Object.defineProperties(Jeygame, {
         "value": function(key) {
             return Jeygame.keysHeld[key];
         }
+    },
+    "fullscreen": {
+        "enumerable": true,
+        "writable": false,
+        "value": function() {
+            if(Jeygame.element) {
+                return Jeygame.element.requestFullscreen();
+                Jeygame.dimensions.x = window.innerWidth;
+                Jeygame.dimensions.y = window.innerHeight;
+            } else {
+                throw new Error(__jg_errors.noRendering);
+            }
+        }
+    },
+    "minimize": {
+        "enumerable": true,
+        "writable": false,
+        "value": function() {
+            if(Jeygame.element) {
+                return document.exitFullscreen();
+            } else {
+                throw new Error(__jg_errors.noRendering);
+            }
+        }
+    },
+    "resizeToFit": {
+        "enumerable": true,
+        "writable": false,
+        "value": function() {
+            if(Jeygame.element) {
+                document.body.style.setProperty("margin", "0px");
+                Jeygame.element.style.setProperty("width", `${window.innerWidth}px`);
+                Jeygame.element.style.setProperty("height", `${window.innerHeight}px`);
+                Jeygame.dimensions.x = window.innerWidth;
+                Jeygame.dimensions.y = window.innerHeight;
+            } else {
+                throw new Error(__jg_errors.noRendering);
+            }
+        }
+    },
+    "resizeTo": {
+        "enumerable": true,
+        "writable": false,
+        "value": function(x,y) {
+            if(Jeygame.element) {
+                Jeygame.element.style.setProperty("width", `${x}px`);
+                Jeygame.element.style.setProperty("height", `${y}px`);
+                Jeygame.dimensions.x = x;
+                Jeygame.dimensions.y = y;
+            } else {
+                throw new Error(__jg_errors.noRendering);
+            }
+            Jeygame.fire("jeygameResize")
+        }
     }
 });
+
+Jeygame.dimensions = {};
+
+let __jg_x, __jg_y
+Object.defineProperties(Jeygame.dimensions, {
+    "x": {
+        "enumerable": true,
+        get() {
+            return __jg_x
+        },
+        set(v) {
+            __jg_x = v;
+            Jeygame.fire("jeygameResize", __jg_x, __jg_y);
+        }
+    },
+    "y": {
+        "enumerable": true,
+        get() {
+            return __jg_y
+        },
+        set(v) {
+            __jg_y = v;
+            Jeygame.fire("jeygameResize", __jg_x, __jg_y);
+        }
+    }
+})
